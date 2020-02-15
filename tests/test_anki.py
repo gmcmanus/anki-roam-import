f1befc9d142e54ac49fbd2286332f5dbf647123d
gmcmanus <gmcmanus@users.noreply.github.com>
2020-02-15 20:14:11 +1100

    Remove redundant test

--- tests/test_anki.py
+++ tests/test_anki.py
@@ -1,23 +1,10 @@
-import pytest
-
 from anki_roam_import.anki import make_anki_notes
 from anki_roam_import.model import AnkiNote
 
 from tests.test_roam import page, block
 
 
-def test_extract_then_translate_no_source():
-    block_json = block('{cloze} text')
-    page_json = page(block_json, title='title')
-    anki_notes = list(make_anki_notes([page_json]))
-
-    assert anki_notes == [AnkiNote(
-        anki_content='{{c1::cloze}} text',
-        source="Note from Roam page 'title'.",
-    )]
-
-
-def test_extract_then_translate_with_source():
+def test_extract_then_translate():
     source_json = block('source:: reference')
     block_json = block('{cloze} text')
     page_json = page(source_json, block_json, title='title')
