a504b48251619e548903dfda477231c14d8503bb
gmcmanus <gmcmanus@users.noreply.github.com>
2020-02-15 21:13:23 +1100

    Simplify test util

--- tests/util.py
+++ tests/util.py
@@ -1,35 +1,31 @@
 from inspect import signature
-from typing import Optional, Any
-from unittest.mock import create_autospec, call, Mock, DEFAULT, sentinel
+from typing import TypeVar
+from unittest.mock import create_autospec, DEFAULT
 
 
-__all__ = ['mock', 'call', 'when', 'sentinel']
+__all__ = ['mock', 'when']
 
 
-def mock(spec: Any = None, *, instance: Optional[bool] = None, **kwargs) -> Any:
-    if spec is None:
-        if instance is not None:
-            raise ValueError
-        return Mock(**kwargs)
+T = TypeVar('T')
 
-    if instance is None:
-        instance = True
 
+def mock(spec: T, *, instance: bool = True, **kwargs) -> T:
     return create_autospec(spec, spec_set=True, instance=instance, **kwargs)
 
 
+# noinspection PyPep8Naming
 class when:
-    def __init__(self, mock):
-        self.mock = mock
+    def __init__(self, mock_object):
+        self.mock_object = mock_object
 
     def called_with(self, *args, **kwargs):
-        return CalledWith(self.mock, *args, **kwargs)
+        return CalledWith(self.mock_object, *args, **kwargs)
 
 
 class CalledWith:
-    def __init__(self, mock, *args, **kwargs):
-        self.mock = mock
-        self.signature = signature(self.mock)
+    def __init__(self, mock_object, *args, **kwargs):
+        self.mock_object = mock_object
+        self.signature = signature(self.mock_object)
         self.arguments = self._normalized_arguments(*args, **kwargs)
 
     def _normalized_arguments(self, *args, **kwargs):
@@ -38,7 +34,7 @@ class CalledWith:
         return dict(bound_arguments.arguments)
 
     def then_return(self, value):
-        original_side_effect = self.mock.side_effect
+        original_side_effect = self.mock_object.side_effect
 
         if callable(original_side_effect):
             fall_back = original_side_effect
@@ -53,4 +49,4 @@ class CalledWith:
                 return value
             return fall_back(*args, **kwargs)
 
-        self.mock.side_effect = side_effect
+        self.mock_object.side_effect = side_effect
